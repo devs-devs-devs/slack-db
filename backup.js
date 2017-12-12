c7b05317d6329526307b934d3a14cf06cfc94fbb
Ash Taylor <a@zi.vc>
2017-12-12 12:42:11 +0000

    [CRON] 2017-12-12 12:42:10

--- backup.js
+++ backup.js
@@ -5,8 +5,8 @@ const es = require('child_process').execSync;
 const timestamp = new Date().toISOString().split('T').join(' ').split('.')[0];
 const git = '/usr/bin/git';
 const mysqldump = '/usr/bin/mysqldump';
-const dir = '/home/slack/slack-db';
-const sql = `${dir}/slack.sql`;
+const dir = __dirname;
+const sql = `${dir}/${process.env.MYSQL_DB}.sql`;
 
-es(`${mysqldump} --compact -u ${process.env.MYSQL_USER} -p${process.env.MYSQL_PASSWORD} --extended-insert=FALSE --databases slack > ${sql}`);
+es(`${mysqldump} --compact -u ${process.env.MYSQL_USER} -p${process.env.MYSQL_PASSWORD} --extended-insert=FALSE --databases ${process.env.MYSQL_DB} > ${sql}`);
 es(`cd ${dir}; ${git} pull; ${git} add .; ${git} commit -m "[CRON] ${timestamp}"; ${git} push --quiet;`);
