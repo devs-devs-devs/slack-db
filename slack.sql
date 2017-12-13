fb8fd0e0d04a3dc83364514536528e7a445fc0ae
Ash Taylor <a@zi.vc>
2017-12-13 13:25:05 +0000

    [CRON] 2017-12-13 13:25:01

--- slack.sql
+++ slack.sql
@@ -7,6 +7,8 @@ USE `slack`;
 CREATE TABLE `copypasta` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `copypasta` blob NOT NULL,
+  `user` varchar(32) NOT NULL DEFAULT '',
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
